03c47d1d61333a5424d084786262529dc6417370
Luka van Hoeven <82316103+LukaVanHoeven@users.noreply.github.com>
2025-09-09 17:47:18 +0200

    added sequential model with plots and inferrer

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
 # MAR 1a
 
+Names:Bas de Blok,Mykola Chuprynskyy,Melissa Rueca,Luka van Hoeven
+
 Classifier 1: Logistic regression
 
-Classifier 2: Feed forward (Luka)
+Classifier 2: Feed forward
+
+The feed forward Neural Network and its related files are found in the ./sequential folder.
+The train_sequential.py file trains and saves a model on the data in the dialog_acts.dat
+During training it shows its accuracy and after training a confusion matrix is shown.
+
+infer_sequential.py is a file which infers the network with a test sentence. It uses the output types and the tokanizer so this is the same for both the training and the inferring process.
 
 Rule-based:
 
 Cli (Bas)
 
-Majorityclass:
+Majorityclass (done):
+
+Confusion matrix, accuracy
+CLI tool
--- main.py
+++ main.py
@@ -1,33 +1,3 @@
-from typing import List
-from keras.preprocessing.text import Tokenizer
-
-with open('dialog_acts.dat', 'r') as file:
-    data = file.readlines()
-    
-cleaned_data = []
-for index, line in enumerate(data):
-    [text, label] = line.split(maxsplit=1)
-    cleaned_data.append((text, label))
-
-sentence = "" 
-for i in range(len(cleaned_data)):
-    label = cleaned_data[i][0]
-    sentence = sentence + cleaned_data[i][1]
-
-    
-# Source for function: https://en.wikipedia.org/wiki/Bag-of-words_model
-def get_bow(sentence: List[str]) -> None:
-    tokenizer = Tokenizer()
-    tokenizer.fit_on_texts(sentence)
-    sequences = tokenizer.texts_to_sequences(sentence)
-    word_index = tokenizer.word_index 
-    bow = {}
-    for key in word_index:
-        bow[key] = sequences[0].count(word_index[key])
-    print(type(bow))
-    return bow
-bow_entire_data = get_bow(sentence)
-
 
 
 
