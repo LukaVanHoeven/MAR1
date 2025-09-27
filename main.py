747d7d97660c6ea6f894f88d985d68cf2155ea35
Timbaken <bas.deblok@student.hu.nl>
2025-09-27 12:11:33 +0200

    added argparse for allowing changes to preferences, all caps and system delay

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from transition import Transition
-from typing import Callable
-import preference_handler
-import pandas as pd
+from ML_sequential import sequential
 from train_sequential import train_sequential
+
+import preference_handler
+import argparse
+import time
+
+import pandas as pd
+from typing import Callable
+
+
 class Dialogue_management_system:
     """
     A class to manage the dialogue system for restaurant recommendations.
@@ -11,7 +18,10 @@ class Dialogue_management_system:
             self, 
             classifier_func: Callable, 
             transitions: list[Transition], 
-            start_state: str
+            start_state: str,
+            allow_preference_change: bool=False,
+            all_caps: bool=False,
+            system_delay: bool=False
         ):
         """
         The constructor for Dialogue_management_system class.
@@ -23,6 +33,10 @@ class Dialogue_management_system:
         self.classifier = classifier_func
         self.transitions = transitions
         self.current_state = start_state
+
+        self.allow_preference_change = allow_preference_change
+        self.all_caps = all_caps
+        self.system_delay = system_delay
         
         self.available_suggestions = []
         self.gathered_suggestions = False
@@ -72,19 +86,47 @@ class Dialogue_management_system:
         for key, value in statement_parser.items():
             if value is not None:  # only update if we got something useful
                 if key == "pricerange":
-                    self.pricerange = value
+                    if self.pricerange is None or self.allow_preference_change:
+                        self.pricerange = value
+                    else:
+                        self._print(
+                            f"\033[93mYou already selected a pricerange " \
+                            f"({self.pricerange}), and changing it is not " \
+                            f"allowed.\033[0m"
+                        )
                 elif key == "area":
-                    self.area = value
+                    if self.area is None or self.allow_preference_change:
+                        self.area = value
+                    else:
+                        self._print(
+                            f"\033[93mYou already selected an area " \
+                            f"({self.area}), and changing it is not " \
+                            f"allowed.\033[0m"
+                        )
                 elif key == "food":
-                    self.food = value
+                    if self.food is None or self.allow_preference_change:
+                        self.food = value
+                    else:
+                        self._print(
+                            f"\033[93mYou already selected a food " \
+                            f"({self.food}), and changing it is not " \
+                            f"allowed.\033[0m"
+                        )
                 elif key == "additional":
-                    self.additional = value
+                    if self.additional is None or self.allow_preference_change:
+                        self.additional = value
+                    else:
+                        self._print(
+                            f"\033[93mYou already selected additional preferences " \
+                            f"({self.additional}), and changing it is not " \
+                            f"allowed.\033[0m"
+                        )
 
     def loop(self):
         """
         This function starts the dialogue loop, allowing the user to interact with the system.
         """
-        print("\033[93mHello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?\033[0m")
+        self._print("\033[93mHello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?\033[0m")
         while True:
             
             user = input(">").lower()
@@ -113,47 +155,77 @@ class Dialogue_management_system:
         """
         match self.current_state, repeat:
             case "welcome", False:
-                print("\033[93mHello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?\033[0m")
+                self._print("\033[93mHello , welcome to the Cambridge restaurant system? You can ask for restaurants by area , price range or food type . How may I help you?\033[0m")
             case "ask_area", False:
-                print("\033[93mIn which area would you like to eat?\033[0m")
+                self._print("\033[93mIn which area would you like to eat?\033[0m")
             case "ask_pricerange", False:
-                print("\033[93mWhat price range are you looking for?\033[0m")
+                self._print("\033[93mWhat price range are you looking for?\033[0m")
             case "ask_food", False:
-                print("\033[93mWhat kind of food would you like?\033[0m")
+                self._print("\033[93mWhat kind of food would you like?\033[0m")
             case "give_suggestion", False:
                 if len(self.available_suggestions) > 0:
                     suggestion = self.available_suggestions.pop(0)
-                    print(f"\033[93mI have found a restaurant that matches your preferences. It is {suggestion} food in the {self.area} area with a {self.pricerange} price range.\033[0m")
+                    self._print(f"\033[93mI have found a restaurant that matches your preferences. It is {suggestion} food in the {self.area} area with a {self.pricerange} price range.\033[0m")
                 else:
-                    print("\033[93mI am sorry, I do not have any more suggestions that match your criteria.\033[0m")
+                    self._print("\033[93mI am sorry, I do not have any more suggestions that match your criteria.\033[0m")
             case "pick_suggested_or_restart", False:
-                print("\033[93mUnfortunately there are no other restaurants matching your criteria. You can either pick the suggested restaurant or start over. Would you like to pick the suggested restaurant?\033[0m")
+                self._print("\033[93mUnfortunately there are no other restaurants matching your criteria. You can either pick the suggested restaurant or start over. Would you like to pick the suggested restaurant?\033[0m")
             case "end_conversation", False:
-                print("\033[93mThank you for using the Cambridge restaurant system. Goodbye!\033[0m")
+                self._print("\033[93mThank you for using the Cambridge restaurant system. Goodbye!\033[0m")
             case "welcome", True:
-                print("\033[93mI am sorry, I did not understand that. How may I help you?\033[0m")
+                self._print("\033[93mI am sorry, I did not understand that. How may I help you?\033[0m")
             case "ask_area", True:
-                print("\033[93mI am sorry, I did not understand that. In which area would you like to eat?\033[0m")
+                self._print("\033[93mI am sorry, I did not understand that. In which area would you like to eat?\033[0m")
             case "ask_pricerange", True:
-                print("\033[93mI am sorry, I did not understand that. What price range are you looking for (cheap, moderate or expensive)?\033[0m")
+                self._print("\033[93mI am sorry, I did not understand that. What price range are you looking for (cheap, moderate or expensive)?\033[0m")
             case "ask_food", True:
-                print("\033[93mI am sorry, we do not have that kind of food. What kind of food would you like?\033[0m")
+                self._print("\033[93mI am sorry, we do not have that kind of food. What kind of food would you like?\033[0m")
             case "give_suggestion", True:
                 if len(self.available_suggestions) > 0:
                     suggestion = self.available_suggestions.pop(0)
-                    print(f"\033[93mI have found another restaurant that matches your preferences called {suggestion}. It is {self.food} food in the {self.area} area with a {self.pricerange} price range.\033[0m")
+                    self._print(f"\033[93mI have found another restaurant that matches your preferences called {suggestion}. It is {self.food} food in the {self.area} area with a {self.pricerange} price range.\033[0m")
                 else:
-                    print("\033[93mI am sorry, I did not understand that. Unfortunately there are no other restaurants matching your criteria.\033[0m")
+                    self._print("\033[93mI am sorry, I did not understand that. Unfortunately there are no other restaurants matching your criteria.\033[0m")
             case "pick_suggested_or_restart", True:
-                print("\033[93mI am sorry, I did not understand that. Unfortunately there are no other restaurants matching your criteria. Will you accept the given suggestion or start over?\033[0m")
+                self._print("\033[93mI am sorry, I did not understand that. Unfortunately there are no other restaurants matching your criteria. Will you accept the given suggestion or start over?\033[0m")
             case "end_conversation", True:
-                print("\033[93mI am sorry, I did not understand that. Thank you for using the Cambridge restaurant system. Goodbye!\033[0m")
-
+                self._print("\033[93mI am sorry, I did not understand that. Thank you for using the Cambridge restaurant system. Goodbye!\033[0m")
+        
+    def _print(self, msg: str):
+        """
+        
+        """
+        if self.all_caps:
+            msg = msg.upper()
+        if self.system_delay:
+            time.sleep()
+        print(msg)
 if __name__ == "__main__":
-    from ML_sequential import sequential
-    import argparse
+    
     parser = argparse.ArgumentParser(description="Run the Dialogue Management System.")
-    parser.add_argument("--train", action="store_true", help="Train the model before running the dialogue system")
+    parser.add_argument(
+        "--train",
+        action="store_true",
+        help="Train the model before running the dialogue system"
+    )
+    parser.add_argument(
+        "--allow-preference-change",
+        action="store_true",
+        default=False,
+        help="Allow the user to adjust their preferences (default: False)"
+    )
+    parser.add_argument(
+        "all-caps",
+        action="store_true",
+        default=False,
+        help="Output the response of the system in all caps (default: False)"
+    )
+    parser.add_argument(
+        "system-delay",
+        action="store_true",
+        default=False,
+        help="Add a 1 second delay before each system response (default: False)"
+    )
     args = parser.parse_args()
 
     transitions = [
@@ -206,6 +278,13 @@ if __name__ == "__main__":
         orig_train = pd.read_csv("train_orig.csv")
         model_sequential_orig, tokenizer_sequential_orig = train_sequential("sequential_orig", orig_train)
     
-    dms = Dialogue_management_system(sequential, transitions, "welcome")
+    dms = Dialogue_management_system(
+        sequential,
+        transitions,
+        "welcome",
+        args.allow_preference_change,
+        args.all_caps,
+        args.system_delay
+    )
     dms.loop()
     
