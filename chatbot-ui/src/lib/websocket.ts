64182cf7336d8c0b7769b1fab8d9fe50bee66a3b
Luka van Hoeven <82316103+LukaVanHoeven@users.noreply.github.com>
2025-10-07 19:40:24 +0200

    buid and correct url i hope

--- chatbot-ui/src/lib/websocket.ts
+++ chatbot-ui/src/lib/websocket.ts
@@ -4,7 +4,7 @@ class WebSocketSingleton {
   public socket: WebSocket;
 
   private constructor() {
-    this.socket = new WebSocket("wss://restaurant.lukahoef.nl/ws");
+    this.socket = new WebSocket("wss://ws.lukahoef.nl");
 
     this.socket.onopen = () => {
       console.log("WebSocket connection established");
